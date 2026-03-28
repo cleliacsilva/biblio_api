51d43c0d88bccd288441d95ecade26bc2e09ad87
cleliacsilva <cleliacsilva@gmail.com>
2026-03-28 14:28:31 -0300

    Modelo de Maturidade de Richardson

---
+++ app/serializers/book_serializer.rb
@@ -0,0 +1,17 @@
+# class BookSerializer < ActiveModel::Serializer
+#   include Rails.application.routes.url_helpers
+
+#   attributes :id, :title, :author, :links
+
+#   def links
+#     {
+#       self: api_nivel3_book_url(object),
+#       update: { href: api_nivel3_book_url(object), method: "PATCH" },
+#       delete: { href: api_nivel3_book_url(object), method: "DELETE" }
+#     }
+#   end
+
+#   def default_url_options
+#     Rails.application.config.action_controller.default_url_options || { host: 'localhost:3000' }
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
+    # Nível 0: O Pântano do POX
+    # Não usamos verbos HTTP nem múltiplas URIs. Existe apenas um "ponto de entrada" (endpoint) que recebe tudo via POST.
+    namespace :nivel0 do
+      post "biblioteca_rpc", to: "rpc#execute"
+    end
+
+    # Nível 1: Recursos (URIs Individuais)
+    # Dividimos o endpoint único em várias URIs, mas ainda usamos os verbos de forma errada (ex: tudo é GET ou tudo é POST).
+    namespace :nivel1 do
+      get "books/get_all", to: "books#get_all"
+      get "books/get_one/:id", to: "books#get_one"
+      get "books/create_new", to: "books#create_new" # Erro semântico: GET para criar
+    end
+
+    # Nível 2: Verbos HTTP + Status Codes
+    # Usa GET para ler, POST para criar, DELETE para apagar e retorna 201 Created ou 404 Not Found.
+    namespace :nivel2 do
       resources :books
     end
-    namespace :v2 do
+
+    # Nível 3: HATEOAS
+    # Além de usar verbos HTTP, cada resposta inclui links para as ações relacionadas
+    namespace :nivel3 do
       resources :books
     end
   end
